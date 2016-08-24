3ca991d0784d57a3989971eb78522f207fdd302a
Vitali Müller <Vitali Mller>
2016-08-24 21:06:00 +0100

    Finished the functionalities to loadd different activities

--- app/src/main/java/com/vitalimueller/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/vitalimueller/flaborfit/DetailsActivity.java
@@ -1,8 +1,10 @@
 package com.vitalimueller.flaborfit;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -14,5 +16,24 @@ public class DetailsActivity extends AppCompatActivity {
 
         TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
         ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseTitle.setText(exTitle);
+
+        if(exTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHT)){
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight,
+                    getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        }else if(exTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)){
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.lotus,
+                    getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916BCD"));
+        }else{
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.heart,
+                    getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
+
     }
 }
