470451cfdf9d75276aada3f35db1adedf583629e
Vitali Müller <Vitali Mller>
2016-08-24 20:50:39 +0100

    created a new detail activity

---
+++ app/src/main/java/com/vitalimueller/flaborfit/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.vitalimueller.flaborfit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImage);
+    }
+}
--- app/src/main/java/com/vitalimueller/flaborfit/MainActivity.java
+++ app/src/main/java/com/vitalimueller/flaborfit/MainActivity.java
@@ -1,13 +1,55 @@
 package com.vitalimueller.flaborfit;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    // final is constant in Java
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
+    public static final String EXERCISE_WEIGHT = "Weight lifting";
+    public static final String EXERCISE_YOGA = "YOGA";
+    public static final String EXERCISE_CARDIO = "CARDIO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightButton = (RelativeLayout)findViewById(R.id.weightBtn);
+        RelativeLayout yogaBtn = (RelativeLayout)findViewById(R.id.yogaBtn);
+        RelativeLayout cardioBtn = (RelativeLayout)findViewById(R.id.cardioBtn);
+
+
+        weightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_WEIGHT);
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_CARDIO);
+            }
+        });
+
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_YOGA);
+            }
+        });
+    }
+
+    private void loadDetailActivity(String exerciseTitle){
+        // (where is it coming from, and where do you want to load)
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exerciseTitle);
+        startActivity(intent);
     }
 }
